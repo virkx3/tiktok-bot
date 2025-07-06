9114ece7cb29eb46e95990d7b149bc3e0e13ba51
Virk <40689073+virkx3@users.noreply.github.com>
2025-07-07 00:41:21 +0530

    Update target.js

--- proxyManager.js
+++ proxyManager.js
@@ -1,18 +1,4 @@
-const fs = require('fs');
-let proxies = [];
-let current = 0;
-
-function loadProxies() {
-  const lines = fs.readFileSync('proxy.txt', 'utf-8').split('\n').filter(Boolean);
-  proxies = lines.filter(p => p.includes(':')); // Only valid proxies
-  current = 0;
-}
-
-function getNextProxy() {
-  if (proxies.length === 0) return null;
-  const proxy = proxies[current];
-  current = (current + 1) % proxies.length;
-  return proxy;
-}
-
-module.exports = { loadProxies, getNextProxy };
+export default [
+  'its.sahiba2233',
+  'iamvirk'
+];
