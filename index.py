bf8287b2600fdbddefb1622c70ceac5066009c12
Virk <40689073+virkx3@users.noreply.github.com>
2025-07-07 11:50:00 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,14 @@
 from login import login_to_tiktok
 from scrape import process_targets
-import time
 
 def main():
     print("[*] Logging into TikTok...")
     browser, context, page = login_to_tiktok()
     
     print("[*] Processing target accounts...")
-    while True:
-        process_targets(page)
-        print("[*] Sleeping for 2 hours before rechecking...")
-        time.sleep(2 * 60 * 60)  # 2 hours
+    process_targets(page)
+
+    browser.close()
 
 if __name__ == "__main__":
     main()
