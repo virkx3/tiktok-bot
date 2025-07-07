c9ebd56bd37a4f79b3f2a075992cb8fae3f559a4
Virk <40689073+virkx3@users.noreply.github.com>
2025-07-07 10:21:09 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,4 @@
 from scrape import scrape
-import time
-from telegram_logger import send_telegram_message
 
 if __name__ == "__main__":
-    send_telegram_message("🚀 TikTok bot container started successfully.")
-    while True:
-        try:
-            scrape()
-        except Exception as e:
-            send_telegram_message(f"❌ Bot crashed with error: {e}")
-        time.sleep(2 * 60 * 60)  # Re-run every 2 hours
+    scrape()
