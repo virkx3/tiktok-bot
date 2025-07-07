9d21837ee0082c4373f930983927370c144dda96
Virk <40689073+virkx3@users.noreply.github.com>
2025-07-07 08:20:49 +0530

    Delete tiktok-login.js

--- tiktok-login.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function login(page) {
-  await page.goto('https://www.tiktok.com/login/phone-or-email/email', { timeout: 60000 });
-  await page.fill('input[name="email"]', 'sociaixzl3s');
-  await page.fill('input[name="password"]', 'Virksaab@12345');
-  await page.click('button:has-text("Log in")');
-  await page.waitForTimeout(5000);
-};
