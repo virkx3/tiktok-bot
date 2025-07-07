f9b466f6102192f57214864272b2e9242d302d29
Virk <40689073+virkx3@users.noreply.github.com>
2025-07-07 08:08:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+TELEGRAM_BOT_TOKEN = '7596985533:AAHjRG1gvHkm2bM6oSJtgOMffHSM8TcgQkw'
+TELEGRAM_USER_ID = '1098100073'
+
+TIKTOK_USERNAMES = ['its.sahiba2233', 'iamvirk']
+TIKTOK_LOGIN = {
+    'username': 'sociaixzl3s',
+    'password': 'Virksaab@12345'
+}
+
+PROXY_FILE = 'proxy.txt'
+RECHECK_INTERVAL_HOURS = 2
+
+SHARE_RULES = [
+    (0, 100, 0),
+    (100, 1000, 50),
+    (1000, 5000, 100),
+    (5000, float('inf'), 150)
+]
